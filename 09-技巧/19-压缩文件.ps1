ce81ea0e937a0c49233276a498918bd5da8c2e9e
fengyehong123 <1355930128@qq.com>
2025-08-18 18:12:01 +0900

    压缩文件

--- 09-技巧/19-压缩文件.ps1
+++ 09-技巧/19-压缩文件.ps1
@@ -2,13 +2,18 @@
     # 目标文件夹
     [string]$TargetFolderPath = ".", 
     # 是否循环压缩
-    [string]$forEachCompressFlag = "",
+    [string]$forEachCompressFlag,
     # 压缩包的默认密码
-    [string]$Passwd = "nrikddi"
+    [string]$Passwd = "123456"
 )
 
-# WinRAR 安装路径
-$CompressionSoft = "C:\软件\压缩软件\WinRAR.exe"
+# ★★★★★★压缩软件的安装路径和种类★★★★★★
+# + -----------------------------------------------------
+# $CompressionSoft = "C:\软件\压缩软件\WinRAR.exe"
+# $CompressionSoftCategory = "WinRAR"
+$CompressionSoft = "E:\soft\7-Zip\7z.exe"
+$CompressionSoftCategory = "7z"
+# + -----------------------------------------------------
 
 if (-not (Test-Path $CompressionSoft)) {
     Write-Host "错误：未找到指定的压缩软件, 请检查安装路径！" -ForegroundColor Red
@@ -19,8 +24,10 @@ if (-not (Test-Path $CompressionSoft)) {
 function Invoke-Compression {
 
     param (
+        # .Net的数据类型, 必须要写全数据类型
         [System.IO.FileSystemInfo]$ItemFileSystem,
-        [string]$Passwd
+        [string]$Passwd,
+        [string]$CompressionSoftCategory
     )
 
     # 判断种类
@@ -38,15 +45,27 @@ function Invoke-Compression {
         return
     }
 
-    # 构建压缩软件所用到的参数
-    $CommandArgs = @(
-        "a",                          # 添加到压缩包
-        "-afzip",                     # 指定为 zip 格式
-        "-ep1",                       # 不包含根目录
-        "-p${Passwd}",                # 压缩密码
-        "`"${ZipName}`"",             # 输出的 zip 文件（带引号）
-        "`"$($ItemFileSystem.FullName)`""   # 被压缩的文件或目录（带引号）
-    )
+    # 根据压缩软甲的种类指定压缩参数
+    $CommandArgs = $null
+    if ("WinRAR" -eq $CompressionSoftCategory) {
+        $CommandArgs = @(
+            "a",                                # 添加到压缩包
+            "-afzip",                           # 指定为 zip 格式
+            "-ep1",                             # 不包含根目录
+            "-p${Passwd}",                      # 压缩密码
+            "`"${ZipName}`"",                   # 输出的 zip 文件（带引号）
+            "`"$($ItemFileSystem.FullName)`""   # 被压缩的文件或目录（带引号）
+        )
+    } elseif ("7z" -eq $CompressionSoftCategory) {
+        $CommandArgs = @(
+            "a",                                # 添加到压缩包
+            "`"$ZipName`"",                     # 输出的 zip 文件（带引号）
+            "`"$($ItemFileSystem.FullName)`"",  # 被压缩的文件或目录（带引号）
+            "-tzip",                            # 指定为 zip 格式
+            "-p${Passwd}",                      # 压缩密码
+            "-r"                                # 递归压缩子目录
+        )
+    }
 
     # 执行压缩命令
     & $CompressionSoft @CommandArgs
@@ -55,7 +74,9 @@ function Invoke-Compression {
 # 如果需要循环压缩的话
 if ($forEachCompressFlag -and "forEachCompressFlag" -eq $forEachCompressFlag) {
     Get-ChildItem -LiteralPath $TargetFolderPath | ForEach-Object {
-        Invoke-Compression -ItemFileSystem $_ -Passwd $Passwd
+        Invoke-Compression -ItemFileSystem $_ `
+                           -Passwd $Passwd `
+                           -CompressionSoftCategory $CompressionSoftCategory
     }
     exit 0;
 }
@@ -63,6 +84,8 @@ if ($forEachCompressFlag -and "forEachCompressFlag" -eq $forEachCompressFlag) {
 # 如果只压缩单个文件或者文件夹的话
 # $TargetFolderPath 只是一个字符串, 通过【Get-Item】获取一下, 转化为[System.IO.FileSystemInfo]类型
 [System.IO.FileSystemInfo]$ItemFileSystem = Get-Item -LiteralPath "$TargetFolderPath"
-Invoke-Compression -ItemFileSystem $ItemFileSystem -Passwd "${Passwd}"
+Invoke-Compression -ItemFileSystem $ItemFileSystem `
+                   -Passwd "${Passwd}" `
+                   -CompressionSoftCategory $CompressionSoftCategory
 
 
