181ae8acb0ad7b2b4344eed0482fafa7956f437d
fengyehong123 <1355930128@qq.com>
2025-09-04 22:23:30 +0900

    判断要运行的命令是否存在

---
+++ 09-技巧/02-脚本执行/03-判断要运行的命令是否存在.ps1
@@ -0,0 +1,8 @@
+﻿# 有python已经安装到环境中, 所以并不会进入此分支
+if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
+    Write-Host "python命令并不存在"
+}
+
+if (-not (Get-Command Hello_Test -ErrorAction SilentlyContinue)) {
+    Write-Host "Hello_Test命令并不存在"  # Hello_Test命令并不存在
+}
