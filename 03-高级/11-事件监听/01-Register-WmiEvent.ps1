08bca6bed9d93908a3d8477f9e1b29ee6a7a69d3
fengyehong123 <1355930128@qq.com>
2026-02-25 21:08:57 +0900

    事件

---
+++ 03-高级/11-事件监听/01-Register-WmiEvent.ps1
@@ -0,0 +1,42 @@
+﻿using namespace System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms
+
+<#
+    Win32_VolumeChangeEvent
+        EventType = 2  设备插入
+        EventType = 3  设备移除
+#>
+Register-WmiEvent `
+    -Query "SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2" `
+    -SourceIdentifier "USBPopup" `
+    -Action {
+
+        # 获取设备ID
+        $eventData = $Event.SourceEventArgs.NewEvent
+        $driveID = $eventData.DriveName
+        if (-not $driveID) {
+            return
+        }
+
+        # 获取所有的盘符之后, 根据设备ID进行过滤
+        $disk = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID='$driveID'"
+        if (-not $disk) {
+            return
+        }
+
+        # 如果当前设备类型不是可移动磁盘的话
+        if ($disk.DriveType -ne 2) {
+            return
+        }
+
+        [MessageBox]::Show(
+            "检测到U盘插入: $driveID",
+            "USB 提示",
+            "OK",
+            "Information"
+        )
+    }
+
+while ($true) {
+    Start-Sleep 5
+}
---
+++ 09-技巧/02-脚本执行/04-脚本退出时执行.ps1
@@ -0,0 +1,11 @@
+﻿# 注册一个 PowerShell 退出事件, 当使用【Ctrl + C】退出脚本的时候, 执行
+Register-EngineEvent PowerShell.Exiting -Action {
+    Write-Host "Ctrl + C 取消成功, 按下Enter键退出脚本..."
+    Pause
+    exit
+}
+
+while ($true) {
+    Write-Host "当前的时间为: $(Get-Date -Format HH:mm:ss)`n按 Ctrl + C 退出`n"
+    Start-Sleep -Seconds 3
+}
