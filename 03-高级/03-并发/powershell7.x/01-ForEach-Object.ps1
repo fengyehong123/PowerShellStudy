e8e5895f808401332d1ecdd08e0b11c0a013508b
fengyehong123 <1355930128@qq.com>
2026-03-04 08:47:26 +0900

    多线程

---
+++ 03-高级/03-并发/powershell7.x/01-ForEach-Object.ps1
@@ -0,0 +1,39 @@
+﻿<#
+    💥ForEach-Object -Parallel 是 powershell7 支持的功能
+#>
+
+# 开启5个线程去执行任务
+1..5 | ForEach-Object -Parallel {
+
+    Start-Sleep -Seconds 2
+    Write-Host "任务 $_ 完成 - 线程ID: $([System.Threading.Thread]::CurrentThread.ManagedThreadId)"
+    <#
+        任务 2 完成 - 线程ID: 27
+        任务 3 完成 - 线程ID: 20
+        任务 1 完成 - 线程ID: 33
+        任务 5 完成 - 线程ID: 15
+        任务 4 完成 - 线程ID: 19
+    #>
+}
+Write-Host '---------------------------------------------'
+
+# 控制并发的数量, 同时最多运行 3 个线程
+1..5 | ForEach-Object -Parallel {
+    Start-Sleep -Seconds 3
+    "任务 $_ 完成"
+} -ThrottleLimit 3
+Write-Host '---------------------------------------------'
+
+# 并行块里不能直接访问外部变量，要用 $using: 来访问
+$delay = 2
+$prefix = "任务"
+$names = @("张三","李四","王五")
+1..3 | ForEach-Object -Parallel {
+
+    Start-Sleep $using:delay
+    Write-Host "$using:prefix $_"
+
+    $using:names | ForEach-Object {
+        Write-Host "$_"
+    }
+}
---
+++ 03-高级/03-并发/powershell7.x/02-Start-ThreadJob.ps1
@@ -0,0 +1,39 @@
+﻿<#
+    💥Start-ThreadJob 是 powershell7 支持的功能
+#>
+
+$temp = "Test"
+$jobs = 1..5 | ForEach-Object {
+
+    # 使用 -ArgumentList 向脚本块中传递参数
+    Start-ThreadJob -ScriptBlock {
+        param($num1, $num2)
+        Start-Sleep 2
+        # 也可以使用 $using: 来使用外部的变量
+        "完成 $num1 $num2 $using:temp"
+    } -ArgumentList $_,10
+}
+
+# 等待完成
+$jobs | Wait-Job
+
+# 获取结果
+$jobs | Receive-Job
+
+# 🔷参数哈希表
+$paramHash = @{
+    Name = "张三"
+    Age  = 20
+    City = "Tokyo"
+}
+
+$job = Start-ThreadJob -ScriptBlock {
+    param($data)
+
+    "姓名: $($data.Name)"
+    "年龄: $($data.Age)"
+    "城市: $($data.City)"
+} -ArgumentList $paramHash
+
+$job | Wait-Job
+$job | Receive-Job
