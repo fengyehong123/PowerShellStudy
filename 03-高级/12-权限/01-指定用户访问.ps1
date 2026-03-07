382e66fe4a59e6f57a26926238f98feb3044f22a
fengyehong123 <1355930128@qq.com>
2026-03-07 16:25:14 +0900

    指定用户访问

---
+++ 03-高级/12-权限/01-指定用户访问.ps1
@@ -0,0 +1,24 @@
+﻿# 查看当前电脑的所有本地用户
+Get-LocalUser
+
+# 创建文件夹
+$path = "D:\PrivateFolder"
+New-Item -ItemType Directory -Path $path -Force | Out-Null
+
+# ###############################
+# 禁用继承并删除继承权限
+# icacls $path /inheritance:r
+# ###############################
+
+# 禁用继承但保留原有权限
+icacls $path /inheritance:d | Out-Null
+
+# 删除 B 用户权限
+# icacls $path /remove "UserB"
+
+# 给指定的用户完全的权限
+icacls $path /grant "贾铭威:(OI)(CI)F" | Out-Null
+
+# 删除默认用户组
+icacls $path /remove "Users" | Out-Null
+icacls $path /remove "Authenticated Users" | Out-Null
