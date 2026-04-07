fb53bc86b05869fad08b4d4c42b4bbc82aa64d57
fengyehong123 <1355930128@qq.com>
2026-04-08 08:02:20 +0900

    上帝模式文件夹

---
+++ 09-技巧/03-系统/02-创建上帝模式文件夹.ps1
@@ -0,0 +1,20 @@
+﻿# 获取桌面路径
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+# 上帝模式文件夹名称
+$godModeName = "GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+# 拼接完整路径
+$godModePath = Join-Path $desktopPath $godModeName
+
+# 判断是否已存在
+if (Test-Path $godModePath) {
+    Write-Host "上帝模式文件夹已经存在：" -ForegroundColor Yellow
+    Write-Host $godModePath
+    Pause
+    return
+}
+
+# 创建文件夹
+New-Item -ItemType Directory -Path $godModePath | Out-Null
+Write-Host "上帝模式创建成功！" -ForegroundColor Green
+Write-Host $godModePath
---
+++ 09-技巧/05-软件/01-Excel/COM对象/04-内存消耗图.ps1
@@ -0,0 +1,19 @@
+﻿# create new excel instance
+$objExcel = New-Object -comobject Excel.Application
+$objExcel.Visible = $True
+$objWorkbook = $objExcel.Workbooks.Add()
+$objWorksheet = $objWorkbook.Worksheets.Item(1)
+
+# write information to the excel file
+$i = 0
+$first10 = (Get-Process | Sort-Object ws -Descending | Select-Object -first 10)
+$first10 | ForEach-Object -Process {$i++; $objWorksheet.Cells.Item($i,1) = $_.name; $objWorksheet.Cells.Item($i,2) = $_.ws}
+$otherMem = (Get-Process | Measure-Object ws -s).Sum - ($first10 | Measure-Object ws -s).Sum
+$objWorksheet.Cells.Item(11,1) = "Others"; $objWorksheet.Cells.Item(11,2) = $otherMem
+
+# draw the pie chart
+$objCharts = $objWorksheet.ChartObjects()
+$objChart = $objCharts.Add(0, 0, 500, 300)
+$objChart.Chart.SetSourceData($objWorksheet.range("A1:B11"), 2)
+$objChart.Chart.ChartType = 70
+$objChart.Chart.ApplyDataLabels(5)
