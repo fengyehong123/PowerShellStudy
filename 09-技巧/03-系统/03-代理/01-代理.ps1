11ad29ce1611afc67360aa203f5be271fb987c3f
fengyehong123 <1355930128@qq.com>
2026-06-13 09:01:08 +0900

    代理

---
+++ 09-技巧/03-系统/03-代理/01-代理.ps1
@@ -0,0 +1,17 @@
+﻿<#
+    Windows 里实际上有两套代理配置
+        1. IE / Internet Options 代理：Edge、Chrome（默认）、Office 等
+        2. WinHTTP 代理              ：Windows Update、部分系统服务、PowerShell 某些场景
+#>
+
+
+# 读取IE代理
+Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+
+# 查看当前 WinHTTP 配置
+netsh winhttp show proxy
+netsh winhttp reset proxy
+
+# 将当前用户在Internet Explorer(实际上是 Windows Internet Options 中配置的代理) 导入到 WinHTTP 代理配置中
+netsh winhttp import proxy source=ie
+netsh winhttp set proxy proxy-server="proxy.company.com:8080"
