f8ac6d1c1136175bdd4d625e21824afd220e23b7
fengyehong123 <1355930128@qq.com>
2026-06-23 20:28:19 +0900

    profile

--- 01-基础/99-启动环境/02-profile.ps1
+++ 01-基础/99-启动环境/02-profile.ps1
@@ -9,7 +9,7 @@ function ll {
 }
 
 # ===============================
-# 设置提示符
+# 设置提示符 Powershell7.x版本使用
 #   ↓↓↓↓
 # PS [FENGYEHONG-HP] Desktop $
 # ===============================
@@ -21,4 +21,42 @@ function prompt {
     $folder   = Split-Path (Get-Location) -Leaf
 
     return "PS `e[32;1m[$hostname]`e[0m `e[36;1m$folder`e[0m $ "
+}
+
+# ===========================================
+# 设置提示符 Powershell5.x 和 7.x版本都可使用
+#   ↓↓↓↓
+# PS [FENGYEHONG-HP] Desktop $
+# ===========================================
+function prompt {
+
+    # PowerShell 7 才支持的 ANSI 转义序列 (e)
+    # 而 PowerShell 5.1 中需要使用 [char]27（ESC 字符）来代替
+    $esc = [char]27
+
+    $hostname = $env:COMPUTERNAME
+    $folder   = Split-Path (Get-Location) -Leaf
+
+    return "PS ${esc}[32;1m[$hostname]${esc}[0m ${esc}[36;1m$folder${esc}[0m $ "
+}
+
+<#
+    每次 PowerShell 等待用户输入命令时, 都会执行 prompt
+    然后
+        1. 函数中所有 Write-Host 输出会立即显示。
+        2. 函数返回的字符串会作为最后的提示符显示。
+#>
+function prompt {
+
+    $hostname = $env:COMPUTERNAME
+    $folder   = Split-Path (Get-Location) -Leaf
+
+    # 拼接信息
+    Write-Host "PS " -NoNewline
+    Write-Host "[$hostname]" -ForegroundColor Green -NoNewline
+    Write-Host " " -NoNewline
+    Write-Host "$folder" -ForegroundColor Cyan -NoNewline
+    Write-Host " " -NoNewline
+
+    return '$ '
 }
