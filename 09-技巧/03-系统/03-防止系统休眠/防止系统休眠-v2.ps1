dbcd9f3da0766f43d1d694e541b65998b2c90d6d
fengyehong123 <1355930128@qq.com>
2026-08-13 09:50:51 +0900

    系统凭据

--- 09-技巧/03-系统/03-防止系统休眠/防止系统休眠-v2.ps1
+++ 09-技巧/03-系统/03-防止系统休眠/防止系统休眠-v2.ps1
@@ -3,8 +3,11 @@
 # 创建Wscript对象
 $shell = New-Object -com "Wscript.Shell"
 while($true){
+
     # 输入一个点, 模拟用户正在输入
+    # 如果用户正在输入内容的话, 也确实会输入一个点, 慎用
     $shell.sendkeys(".")
+
     # 模拟按下不存在的F15键
     [System.Windows.Forms.SendKeys]::SendWait("{F15}")
     # 睡眠1分钟
---
+++ 09-技巧/03-系统/10-系统凭据/README.md
@@ -0,0 +1,6 @@
+1. 从下面这个网址下载凭据查看的第三方软件
+https://www.nirsoft.net/utils/credentials_file_view.html
+
+2. 解压之后, 使用管理员权限运行`CredentialsFileView.exe`
+
+3. 然后参照`Step1.png`和`Step2.png`设置后, 查看密码即可
